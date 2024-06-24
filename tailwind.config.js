2249cb3eb5e54cc9ef29d0bc1da8b5a793abb46c
chadnichowdhury <chadni.suchona@gmail.com>
2024-06-25 04:10:44 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'Playwrite': ['Playwrite MX', 'cursive'],
+    },
+    // font-family: "", ;
+
+    extend: {},
+  },
+  plugins: [],
+}
+
